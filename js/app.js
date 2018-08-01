17c6376bde0218984be1f95ee1b89a15acdc5ffc
Herdez <abelquantikum@gmail.com>
2018-08-01 09:28:38 -0500

    Activity Async fetch and render

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function getApi() {
+
+    const request = superagent;
+    var API_URL = 'https://randomuser.me/api/?results=50';
+
+	request
+		.get(API_URL)
+		.then(function(response){
+			var repos = response.body.results;
+			repos.forEach(function(user){
+				console.log(user.email);
+				console.log(user.nat);
+				document.querySelector('.users-list').innerHTML += '<article>' +
+            													   '<span>' + user.nat + '</span>' +
+            													   '<h6>' + user.email + '</h6>' +
+            													   '</article>';
+			});
+		});
+}
+
+getApi();
